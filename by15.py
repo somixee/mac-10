098c2949ecdfe7d6ea1331d7934d260622d0eb24
somixee <somtoc40@gmail.com>
2022-07-25 22:29:13 -0700

    initial commit

---
+++ by15.py
@@ -0,0 +1,25 @@
+
+
+
+import os
+os.system("clear")
+age = 44
+names = ['shola', 'kcee', 'david']
+
+fav_drink ={
+"shola":"zobo",
+ "kcee":"fanta",
+  "emma":"coke",
+  }
+
+fav_drink= True
+
+
+print(fav_drink)
+
+
+
+
+
+
+                                                                                                                                                                              
---
+++ hello.py
@@ -0,0 +1,5 @@
+print ("hello world")
+import os
+
+fuLL_name = "somto collins"
+print(fuLL_name)
